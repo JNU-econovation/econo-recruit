5ab863b305c7d217b6e56fb9177c526ca3ce91fd
이서현 <ymecca12@gmail.com>
2023-02-12 21:23:24 +0900

    build : 서버 기본 개발 환경 구축

---
+++ src/server/src/main/java/com/econovation/recruit/RecruitApplication.java
@@ -0,0 +1,12 @@
+package com.econovation.recruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitApplication.class, args);
+    }
+
+}
---
+++ src/server/src/test/java/com/econovation/recruit/RecruitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.econovation.recruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
