23bccc59034b5c86343ccbd2ad7b9267e0d5978b
bada308 <203190@jnu.ac.kr>
2023-08-17 00:54:57 +0900

    feat: Add mock data

---
+++ frontend/mock/MockData.ts
@@ -0,0 +1,250 @@
+export const KanbanMock = [
+  {
+    id: 1,
+    title: "[기획자] 지원서",
+    column: [
+      {
+        id: 10,
+        major: "소프트웨어공학과",
+        title: "[기획자] 임채승",
+        apply: ["APP", "WEB"],
+        comment: 14,
+        heart: 6,
+        isHearted: true,
+      },
+      {
+        id: 2,
+        major: "소프트웨어공학과",
+        title: "[기획자] 임채승",
+        apply: ["APP", "WEB"],
+        comment: 14,
+        heart: 6,
+        isHearted: false,
+      },
+      {
+        id: 11,
+        major: "산업공학과",
+        title: "[기획자] 김지민",
+        apply: ["APP", "WEB"],
+        comment: 14,
+        heart: 6,
+        isHearted: false,
+      },
+      {
+        id: 12,
+        major: "산업공학과",
+        title: "[기획자] 김지민",
+        apply: ["APP", "WEB"],
+        comment: 14,
+        heart: 6,
+        isHearted: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "[개발자] 지원서",
+    column: [
+      {
+        id: 3,
+        major: "소프트웨어공학과",
+        title: "[개발자] 이서현",
+        apply: ["APP", "WEB"],
+        comment: 14,
+        heart: 6,
+        isHearted: true,
+      },
+      {
+        id: 4,
+        major: "소프트웨어공학과",
+        title: "[개발자] 이서현",
+        apply: ["APP", "WEB"],
+        comment: 14,
+        heart: 6,
+        isHearted: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "[디자이너] 지원서",
+    column: [
+      {
+        id: 5,
+        major: "소프트웨어공학과",
+        title: "[디자이너] 김서하",
+        apply: ["APP", "WEB"],
+        comment: 14,
+        heart: 6,
+        isHearted: true,
+      },
+      {
+        id: 6,
+        major: "소프트웨어공학과",
+        title: "[디자이너] 김서하",
+        apply: ["APP", "WEB"],
+        comment: 14,
+        heart: 6,
+        isHearted: false,
+      },
+    ],
+  },
+];
+
+export const ApplicantCommentMock = [
+  {
+    id: 1,
+    author: "22기 박수현",
+    date: "2033.02.21",
+    content:
+      "지원서, 기획서, 포트폴리오 모두 정말 정성적으로 작성해주신 점 좋았습니다. 현재 병행하고 계신 활동, 특히 동아리 관련 활동이 많으신데 실질적으로 에코노베이션에 할애할 수 있는 시간이 어느정도일지 궁금합니다.",
+    like: 2,
+    checked: true,
+  },
+  {
+    id: 2,
+    author: "22기 박수현",
+    date: "2033.02.21",
+    content:
+      "지원서, 기획서, 포트폴리오 모두 정말 정성적으로 작성해주신 점 좋았습니다. 현재 병행하고 계신 활동, 특히 동아리 관련 활동이 많으신데 실질적으로 에코노베이션에 할애할 수 있는 시간이 어느정도일지 궁금합니다.",
+    like: 4,
+    checked: false,
+  },
+  {
+    id: 3,
+    author: "22기 김서하",
+    date: "2033.02.21",
+    content:
+      "지원서, 기획서, 포트폴리오 모두 정말 정성적으로 작성해주신 점 좋았습니다. 현재 병행하고 계신 활동, 특히 동아리 관련 활동이 많으신데 실질적으로 에코노베이션에 할애할 수 있는 시간이 어느정도일지 궁금합니다.",
+    like: 2,
+    checked: false,
+  },
+];
+
+export const ApplicationListMock = [
+  {
+    title: "[개발자] 임채승",
+    apply: ["APP", "WEB"],
+    semester: "1학년 1학기",
+    registerDate: "2023.03.31",
+  },
+  {
+    title: "[개발자] 임채승",
+    apply: ["APP", "WEB"],
+    semester: "1학년 1학기",
+    registerDate: "2023.03.31",
+  },
+  {
+    title: "[개발자] 임채승",
+    apply: ["APP", "WEB"],
+    semester: "1학년 1학기",
+    registerDate: "2023.03.31",
+  },
+  {
+    title: "[개발자] 임채승",
+    apply: ["APP", "WEB"],
+    semester: "1학년 1학기",
+    registerDate: "2023.03.31",
+  },
+  {
+    title: "[개발자] 임채승",
+    apply: ["APP", "WEB"],
+    semester: "1학년 1학기",
+    registerDate: "2023.03.31",
+  },
+  {
+    title: "[개발자] 임채승",
+    apply: ["APP", "WEB"],
+    semester: "1학년 1학기",
+    registerDate: "2023.03.31",
+  },
+  {
+    title: "[개발자] 임채승",
+    apply: ["APP", "WEB"],
+    semester: "1학년 1학기",
+    registerDate: "2023.03.31",
+  },
+];
+
+export const InterviewListMock = [
+  {
+    title: "[개발자] 임채승",
+    apply: ["APP", "WEB"],
+    score: "4.5",
+    registerDate: "2023.03.31",
+  },
+  {
+    title: "[개발자] 임채승",
+    apply: ["APP", "WEB"],
+    score: "4.5",
+    registerDate: "2023.03.31",
+  },
+  {
+    title: "[개발자] 임채승",
+    apply: ["APP", "WEB"],
+    score: "4.5",
+    registerDate: "2023.03.31",
+  },
+  {
+    title: "[개발자] 임채승",
+    apply: ["APP", "WEB"],
+    score: "4.5",
+    registerDate: "2023.03.31",
+  },
+  {
+    title: "[개발자] 임채승",
+    apply: ["APP", "WEB"],
+    score: "4.5",
+    registerDate: "2023.03.31",
+  },
+  {
+    title: "[개발자] 임채승",
+    apply: ["APP", "WEB"],
+    score: "4.5",
+    registerDate: "2023.03.31",
+  },
+  {
+    title: "[개발자] 임채승",
+    apply: ["APP", "WEB"],
+    score: "4.5",
+    registerDate: "2023.03.31",
+  },
+];
+
+export const ManagerListMock = [
+  {
+    name: "임채승",
+    period: 22,
+    type: "captin",
+  },
+  {
+    name: "임채승",
+    period: 22,
+    type: "captin",
+  },
+  {
+    name: "임채승",
+    period: 22,
+    type: "captin",
+  },
+  {
+    name: "임채승",
+    period: 22,
+    type: "captin",
+  },
+  {
+    name: "임채승",
+    period: 22,
+    type: "captin",
+  },
+  {
+    name: "임채승",
+    period: 22,
+    type: "captin",
+  },
+  {
+    name: "임채승",
+    period: 22,
+    type: "captin",
+  },
+];
