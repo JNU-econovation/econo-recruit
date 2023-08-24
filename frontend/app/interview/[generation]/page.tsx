7a481323befbefa183619d006be754a30af07cf9
bada308 <203190@jnu.ac.kr>
2023-08-24 20:17:39 +0900

    fix: fix interview page - generation props로 직접 받기 - useEffect 의존성 배열 추가 - popup component, list component -> board component

--- frontend/app/interview/[generation]/page.tsx
+++ frontend/app/interview/[generation]/page.tsx
@@ -2,8 +2,7 @@
 import PageNavbarComponent from "@/components/common/PageNavbar.component";
 import SortListComponent from "@/components/common/SortList.component";
 import CommonNavbar from "@/components/common/navbar/Navbar.component";
-import InterviewListComponent from "@/components/interview/List.component";
-import InterviewPopupComponent from "@/components/interview/Popup.component";
+import InterviewBoardComponent from "@/components/interview/Board.component";
 import InterviewSearchComponent from "@/components/interview/Search.component";
 import { InterviewListMock } from "@/mock/MockData";
 import { interviewListState } from "@/src/stores/interview/Interview.atom";
@@ -12,13 +11,10 @@ import { useSearchParams } from "next/navigation";
 import { FC, useEffect } from "react";
 
 interface InterviewPageProps {
-  params: {
-    generation: string;
-  };
+  generation: string;
 }
 
-const InterviewPage: FC<InterviewPageProps> = ({ params }) => {
-  const { generation } = params;
+const InterviewPage: FC<InterviewPageProps> = ({ generation }) => {
   const searchParmas = useSearchParams();
   const type = searchParmas.get("type") ?? "list";
   const order = searchParmas.get("order") ?? "newest";
@@ -34,27 +30,24 @@ const InterviewPage: FC<InterviewPageProps> = ({ params }) => {
 
   useEffect(() => {
     setInterviewList(InterviewListMock);
-  });
+  }, []);
 
   return (
-    <>
-      <InterviewPopupComponent />
-      <div className="px-24 w-max-[1280px] flex p-12">
-        <CommonNavbar generation={generation} />
-        <div className="flex-1 ml-32 min-w-[46rem] mb-48">
-          <div className="flex w-full justify-end gap-8 my-12">
-            <InterviewSearchComponent />
-            <SortListComponent sortList={orderMenu} selected={order} />
-          </div>
-          <InterviewListComponent />
-          <PageNavbarComponent
-            maxLength={4}
-            page={+page}
-            url={`/interview/${generation}?type=${type}&order=${order}`}
-          />
+    <div className="px-24 max-w-[1280px] flex p-12">
+      <CommonNavbar generation={generation} />
+      <div className="flex-1 ml-32 min-w-[46rem] mb-48">
+        <div className="flex w-full justify-end gap-8 my-12">
+          <InterviewSearchComponent />
+          <SortListComponent sortList={orderMenu} selected={order} />
         </div>
+        <InterviewBoardComponent />
+        <PageNavbarComponent
+          maxLength={4}
+          page={+page}
+          url={`/interview/${generation}?type=${type}&order=${order}`}
+        />
       </div>
-    </>
+    </div>
   );
 };
 
