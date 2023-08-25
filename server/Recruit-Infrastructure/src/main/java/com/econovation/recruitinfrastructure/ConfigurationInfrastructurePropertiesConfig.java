6ea2b58a142daacc78920c9251bfd44720ce9b8d
이서현 <ymecca12@gmail.com>
2023-08-25 17:01:23 +0900

    feat : SlackApi Setting

---
+++ server/Recruit-Infrastructure/src/main/java/com/econovation/recruitinfrastructure/ConfigurationInfrastructurePropertiesConfig.java
@@ -0,0 +1,10 @@
+package com.econovation.recruitinfrastructure;
+
+import com.econovation.recruitinfrastructure.slack.config.SlackProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@EnableConfigurationProperties({
+        SlackProperties.class})
+@Configuration
+public class ConfigurationInfrastructurePropertiesConfig {}
