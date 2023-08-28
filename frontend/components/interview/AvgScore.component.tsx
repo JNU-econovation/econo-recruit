c69b67b1ab44ded66d0f49d60cb28300181558b9
bada308 <203190@jnu.ac.kr>
2023-08-28 22:45:19 +0900

    refactor: change interview left component - 컴포넌트명 수정 (label -> avgScore, comment -> score) - interface 추가 - props 변경

--- frontend/components/interview/AvgScore.component.tsx
+++ frontend/components/interview/AvgScore.component.tsx
@@ -1,32 +1,35 @@
 import { InterviewScoreMock } from "@/mock/MockData";
+import { ScoreRes, scoreDetail } from "@/src/apis/score";
 import { getScoreAverage } from "@/src/functions/calculator";
+import { FC } from "react";
 
-const InterviewLabelComponent = () => {
-  const interviewScoreList = InterviewScoreMock;
+interface InterviewAvgScoreProps {
+  totalAverage: number;
+  average: scoreDetail[];
+}
 
-  // 서버에서 보내주는 데이터 형식에 따라 수정해야함
-  const scoreAverage = getScoreAverage(
-    interviewScoreList.map((row) => Number(row.score))
-  );
-
+const InterviewAvgScoreComponent: FC<InterviewAvgScoreProps> = ({
+  totalAverage,
+  average,
+}) => {
   return (
     <div className="flex w-full items-center mt-10 gap-[6%]">
       <div className="w-[6.7rem]">
         <span className="text-5xl font-extrabold text-[#333333] relative">
           <div className="absolute w-[6.7rem] h-5 bg-[#A6BFFF] top-9 -z-10"></div>
-          {scoreAverage}
+          {totalAverage}
         </span>
       </div>
       <div className="flex w-full gap-[3%]">
-        {interviewScoreList.map((score) => (
+        {average.map((score) => (
           <div
-            key={score.type}
+            key={score.creteria}
             className="flex flex-col items-center justify-center gap-1 min-w-[4.1rem]"
           >
             <span className="text-2xl font-semibold text-[#4E4E4E] max-w-full">
               {score.score}
             </span>
-            <span className="text-sm text-[#4F4F4F]">{score.string}</span>
+            <span className="text-sm text-[#4F4F4F]">{score.creteria}</span>
           </div>
         ))}
       </div>
@@ -34,4 +37,4 @@ const InterviewLabelComponent = () => {
   );
 };
 
-export default InterviewLabelComponent;
+export default InterviewAvgScoreComponent;
