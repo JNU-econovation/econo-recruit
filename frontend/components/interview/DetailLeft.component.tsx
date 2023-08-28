b15a165e3c5751c0dcb1016fb588ff47bcc3807f
bada308 <203190@jnu.ac.kr>
2023-08-28 22:35:03 +0900

    fix: refact interview left component - interface 추가 - component명 수정

--- frontend/components/interview/DetailLeft.component.tsx
+++ frontend/components/interview/DetailLeft.component.tsx
@@ -1,13 +1,27 @@
-import InterviewCommentComponent from "./Comment.component";
-import InterviewLabelComponent from "./Label.component";
 import InterviewUserComponent from "./User.component";
+import { InterviewRes } from "@/src/apis/interview";
+import { ScoreRes } from "@/src/apis/score";
+import { FC } from "react";
+import InterviewAvgComponent from "@/components/interview/AvgScore.component";
+import InterviewScoreComponent from "./Score.component";
 
-const InterviewDetailLeftComponent = (props: any) => {
+interface InterviewDetailLeftProps {
+  data: InterviewRes;
+  scoreData: ScoreRes;
+}
+
+const InterviewDetailLeftComponent: FC<InterviewDetailLeftProps> = async ({
+  data,
+  scoreData,
+}) => {
   return (
     <>
-      <InterviewUserComponent src="." />
-      <InterviewLabelComponent />
-      <InterviewCommentComponent />
+      <InterviewUserComponent src={data.url} />
+      <InterviewAvgComponent
+        totalAverage={scoreData.totalAverage}
+        average={scoreData.scoreVo.average}
+      />
+      <InterviewScoreComponent score={scoreData} />
     </>
   );
 };
