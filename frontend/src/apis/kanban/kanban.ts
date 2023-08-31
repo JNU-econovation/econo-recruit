6054599b4cd3de6bf5f11ea3410fc4336f4ad6bf
bada308 <203190@jnu.ac.kr>
2023-08-31 22:22:53 +0900

    feat: move kanban api file from api to kanban folder

---
+++ frontend/src/apis/kanban/kanban.ts
@@ -0,0 +1,79 @@
+import { https } from "@/src/functions/axios";
+import {
+  KanbanCardData,
+  KanbanColumnData,
+} from "../../stores/kanban/Kanban.atoms";
+
+export interface KanbanCardReq {
+  id: number;
+  boardId: number;
+  nextBoardId: number;
+  cardType: "WORK_CARD" | "APPLICANT";
+  title: string;
+  content: string;
+  labelCount: number;
+  commentCount: number;
+}
+
+// card api 추가 시 수정 필요
+export const getKanbanCards = async (columnId: string) => {
+  const { data } = await https.get<KanbanCardReq[]>(
+    `/navigations/${columnId}/boards`
+  );
+
+  return data;
+};
+
+interface KanbanNavigationReq {
+  columnsId: number;
+  title: string;
+  navigationId: number;
+}
+
+export const getColums = async (navigationId: string) => {
+  const { data } = await https.get<KanbanNavigationReq[]>(
+    `/boards/navigations/${navigationId}/columns`
+  );
+
+  return data;
+};
+
+// export interface
+interface addColumnReq {
+  navigationId: string;
+  title: string;
+}
+
+export const postAddColumn = async ({ navigationId, title }: addColumnReq) => {
+  const { data } = await https.post<string>(
+    `/boards/navigations/${navigationId}/columns`,
+    null,
+    {
+      params: { title: title },
+    }
+  );
+
+  return data;
+};
+
+export const getAllKanbanData = async (
+  navigationId: string
+): Promise<KanbanColumnData[]> => {
+  const columnsData = await getColums(navigationId);
+  const cardsData = await Promise.all(
+    columnsData.map((column) => getKanbanCards(column.columnsId.toString()))
+  );
+
+  return columnsData.map((column, index) => ({
+    id: column.columnsId,
+    title: column.title,
+    card: cardsData[index].map((card) => ({
+      id: card.id,
+      title: card.title,
+      apply: [] as string[],
+      comment: card.commentCount,
+      heart: card.labelCount,
+      isHearted: false,
+    })),
+  }));
+};
