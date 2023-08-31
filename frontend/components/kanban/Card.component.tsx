3a715b08020915c07b48211368d9ed9f794c1127
loopy-lim <loopy@jnu.ac.kr>
2023-08-31 22:04:11 +0900

    feat: add keys for api

--- frontend/components/kanban/Card.component.tsx
+++ frontend/components/kanban/Card.component.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { KanbanCardData } from "@/src/stores/kanban/Kanban.atoms";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 type KanbanCardComponentType = {
   data: KanbanCardData | null;
@@ -13,11 +13,12 @@ function KanbanCardComponent({ data, cardId }: KanbanCardComponentType) {
     return <></>;
   }
 
-  const { major, title, apply, comment, isHearted, heart, id } = data;
+  const { title, apply, comment, isHearted, heart, id } = data;
   const { generation } = useParams();
+  const navigate = useRouter();
 
   const onClickDetail = () => {
-    window.location.href = `/kanban/${generation}/detail?id=${id}&card=${cardId}`;
+    navigate.push(`/kanban/${generation}/detail?id=${id}&card=${cardId}`);
   };
 
   return (
@@ -25,7 +26,7 @@ function KanbanCardComponent({ data, cardId }: KanbanCardComponentType) {
       className="border-[1px] border-[#F0F0F0] w-full p-3 rounded-lg drop-shadow-md bg-white hover:border-[#7AA0FF]"
       onClick={onClickDetail}
     >
-      <div className="text-xs text-[#666666]">{major}</div>
+      {/* <div className="text-xs text-[#666666]">{major}</div> */}
       <div className="font-bold">{title}</div>
       <div className="mt-2 flex justify-between items-center text-sm text-[#666666]">
         <div className="text-sm">{apply.join(" / ")}</div>
