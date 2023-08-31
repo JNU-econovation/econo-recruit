e79c82c6a2d9e52226abc383ff3fe1497cfdaec9
bada308 <203190@jnu.ac.kr>
2023-08-31 17:38:22 +0900

    feat: change kanban menu constants

--- frontend/src/constants/kanban/26.ts
+++ frontend/src/constants/kanban/26.ts
@@ -1,9 +1,35 @@
-export const KANBAN_MANUES = [
-  '공통',
-  '회장단',
-  '운영팀',
-  '홍보 및 디자인팀',
-  '지원자 대응팀',
-  'OT담당팀',
-  '기타 참고',
+export interface KanbanMenu {
+  id: number;
+  navTitle: string;
+}
+
+export const KANBAN_MENU = [
+  {
+    id: 0,
+    navTitle: "공통",
+  },
+  {
+    id: 1,
+    navTitle: "회장단",
+  },
+  {
+    id: 2,
+    navTitle: "운영팀",
+  },
+  {
+    id: 3,
+    navTitle: "홍보 및 디자인팀",
+  },
+  {
+    id: 4,
+    navTitle: "지원자 대응팀",
+  },
+  {
+    id: 5,
+    navTitle: "OT담당팀",
+  },
+  {
+    id: 6,
+    navTitle: "기타 참고",
+  },
 ];
