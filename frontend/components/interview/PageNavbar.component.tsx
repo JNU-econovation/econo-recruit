33e390466bea6fe98236d9e51876f3759c672dab
loopy-lim <loopy@jnu.ac.kr>
2023-09-03 00:29:12 +0900

    feat: pagenation

--- frontend/components/interview/PageNavbar.component.tsx
+++ frontend/components/interview/PageNavbar.component.tsx
@@ -1,19 +1,44 @@
 "use client";
+import { useQuery } from "@tanstack/react-query";
 import PageNavbarComponent from "../common/PageNavbar.component";
 import { useSearchParams } from "next/navigation";
+import { getApplicantByPage } from "@/src/apis/applicant/applicant";
 
 type InterviewPageNavbarProps = {
   generation: string;
 };
 
 const InterviewPageNavbar = ({ generation }: InterviewPageNavbarProps) => {
-  const searchParmas = useSearchParams();
-  const type = searchParmas.get("type") ?? "list";
-  const order = searchParmas.get("order") ?? "newest";
-  const page = searchParmas.get("page") ?? "1";
+  const searchParams = useSearchParams();
+  const pageIndex = searchParams.get("page") || "1";
+  const type = searchParams.get("type") ?? "list";
+  const order = searchParams.get("order") ?? "newest";
+  const page = searchParams.get("page") ?? "1";
+
+  const {
+    data: allData,
+    isLoading,
+    isError,
+  } = useQuery(
+    ["allApplicant", generation],
+    () => getApplicantByPage(+pageIndex),
+    {
+      enabled: !!generation,
+    }
+  );
+
+  if (!allData || isLoading) {
+    return <div>로딩중...</div>;
+  }
+
+  if (isError) {
+    return <div>에러 발생</div>;
+  }
+
+  const { maxPage } = allData;
   return (
     <PageNavbarComponent
-      maxLength={4}
+      maxLength={maxPage}
       page={+page}
       url={`/interview/${generation}?type=${type}&order=${order}`}
     />
