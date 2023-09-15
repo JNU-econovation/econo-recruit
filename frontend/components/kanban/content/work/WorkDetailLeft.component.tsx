d0b6fda4d0f13d6404e26b731827d6a9346b7b15
loopy-lim <loopy@jnu.ac.kr>
2023-09-16 02:54:36 +0900

    feat: add title change logic at work card

--- frontend/components/kanban/content/work/WorkDetailLeft.component.tsx
+++ frontend/components/kanban/content/work/WorkDetailLeft.component.tsx
@@ -1,8 +1,9 @@
-import { FC } from "react";
-import { Work } from "@/src/apis/work/work";
+import { FC, use, useEffect, useState } from "react";
+import { Work, putWork } from "@/src/apis/work/work";
 import dynamic from "next/dynamic";
 import Txt from "@/components/common/Txt.component";
 import WorkLabel from "./Label.component";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 interface WorkDetailLeftProps {
   cardId: number;
@@ -21,10 +22,59 @@ const WorkDetailLeft: FC<WorkDetailLeftProps> = ({
   generation,
   cardId,
 }) => {
+  const [title, setTitle] = useState("");
+  const [isOpenAddCard, setIsOpenAddCard] = useState(false);
+  const queryClient = useQueryClient();
+
+  const { mutate: changeWorkCardTitle } = useMutation(putWork, {
+    onSettled: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["work", cardId],
+      });
+    },
+  });
+
+  const addCardSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    changeWorkCardTitle({ cardId, title });
+    setIsOpenAddCard(false);
+  };
+
+  useEffect(() => {
+    setTitle(data.title);
+  }, []);
+
   return (
     <>
       <div className="flex flex-col gap-1 mb-8">
-        <Txt typography="h2">{data.title}</Txt>
+        <div className="flex w-full justify-between items-baseline">
+          {isOpenAddCard ? (
+            <form className="flex gap-2" onSubmit={(e) => addCardSubmit(e)}>
+              <input
+                type="text"
+                className="font-bold w-full border rounded-lg p-2"
+                placeholder="Enter a title for this card"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              <div className="flex text-sm gap-2 text-[#666666] items-center w-[4rem]">
+                <button
+                  type="submit"
+                  onClick={() => setIsOpenAddCard((prev) => !prev)}
+                >
+                  수정완료
+                </button>
+              </div>
+            </form>
+          ) : (
+            <Txt typography="h2">{data.title}</Txt>
+          )}
+          <div className="flex text-sm gap-2 text-[#666666] items-center">
+            <button onClick={() => setIsOpenAddCard((prev) => !prev)}>
+              수정
+            </button>
+          </div>
+        </div>
       </div>
       <WorkLabel cardId={cardId} generation={generation} />
       <ApplicantComment cardId={cardId} postId="" generation={generation} />
