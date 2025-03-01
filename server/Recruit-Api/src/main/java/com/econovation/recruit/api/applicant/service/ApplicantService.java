e7ee097a60a55b5d3cfc0187f7a57d0f20a3ed97
westzeroright <kt8267@naver.com>
2025-03-01 19:48:14 +0900

    [refactor]: 중복 임포트문 삭제

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/applicant/service/ApplicantService.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/applicant/service/ApplicantService.java
@@ -22,8 +22,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static com.econovation.recruitcommon.consts.RecruitStatic.PASS_STATE_KEY;
-
 @Service
 @RequiredArgsConstructor
 public class ApplicantService implements ApplicantQueryUseCase {
