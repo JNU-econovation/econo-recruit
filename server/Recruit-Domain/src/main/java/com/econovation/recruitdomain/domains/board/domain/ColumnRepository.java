765efd8a48b745cfb5cd833a5c63b9abb8188c16
injae-348 <ant0593@naver.com>
2025-03-02 19:22:24 +0900

    [refactor]: findByNavigationIdAndYear함수 @Query 추가

--- server/Recruit-Domain/src/main/java/com/econovation/recruitdomain/domains/board/domain/ColumnRepository.java
+++ server/Recruit-Domain/src/main/java/com/econovation/recruitdomain/domains/board/domain/ColumnRepository.java
@@ -3,12 +3,16 @@ package com.econovation.recruitdomain.domains.board.domain;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ColumnRepository extends JpaRepository<Columns, Integer> {
     List<Columns> findByNavigationId(Integer navigationId);
 
+    @Query(
+            "SELECT c FROM Columns c WHERE c.navigationId = :navigationId AND c.year = :year"
+    )
     List<Columns> findByNavigationIdAndYear(Integer navigationId, Integer year);
 
     Optional<Columns> findByNextColumnsIdAndNavigationId(Integer nextColLoc, Integer navigationId);
