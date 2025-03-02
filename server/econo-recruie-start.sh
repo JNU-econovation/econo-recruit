537059f367b465df3949d55b1ecfd94bc1cdec32
rlajm1203 <whdlas011203@naver.com>
2025-03-02 16:23:37 +0900

    feat: 배포 자동화 셸 스크립트 작성

---
+++ server/econo-recruie-start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+cd "$(dirname "$0")"
+
+echo "backed 컨테이너를 삭제합니다.."
+docker rm -f backend
+
+echo "Docker 이미지를 받아옵니다.."
+docker pull blackbean99/econo-recruit:latest
+
+echo "Docker 컨테이너를 실행합니다.."
+docker-compose up -d --remove-orphans
+
+echo "사용하지 않는 도커 이미지를 삭제합니다.."
+docker image prune -a -f
