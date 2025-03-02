42d628a80918437bb0c52312cdc69fc653a95883
injae-348 <ant0593@naver.com>
2025-03-02 19:26:49 +0900

    [feat]: 지원서 칸반보드 열 생성 V2 API 추가
    - BoardRestControllerV2: title과 year를 Body로 받아와 Column을 생성하는
      엔드포인트 추가

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/card/controller/BoardRestControllerV2.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/card/controller/BoardRestControllerV2.java
@@ -1,7 +1,13 @@
 package com.econovation.recruit.api.card.controller;
 
+import static com.econovation.recruitcommon.consts.RecruitStatic.COLUMN_SUCCESS_REGISTER_MESSAGE;
+
+import com.econovation.recruit.api.card.docs.CreateColumnsExceptionDocs;
 import com.econovation.recruit.api.card.usecase.BoardLoadUseCase;
+import com.econovation.recruit.api.card.usecase.BoardRegisterUseCase;
+import com.econovation.recruitcommon.annotation.ApiErrorExceptionsExample;
 import com.econovation.recruitdomain.domains.board.dto.ColumnsResponseDto;
+import com.econovation.recruitdomain.domains.dto.CreateColumnsDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,6 +17,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "[2.0]. 칸반보드 API V2", description = "칸반보드 관련 API")
 public class BoardRestControllerV2 {
     private final BoardLoadUseCase boardLoadUseCase;
+    private final BoardRegisterUseCase boardRecordUseCase;
 
     @Operation(
             summary = "지원서 세로줄 조회(by NavigationId and Year)",
@@ -33,5 +42,17 @@ public class BoardRestControllerV2 {
         return new ResponseEntity<>(
                 boardLoadUseCase.getColumnsByNavigationIdAndYear(navigationId, year),
                 HttpStatus.OK);
+    }
+
+    @Operation(
+            summary = "지원서 칸반보드 열(세로줄) 생성",
+            description = "지원서 칸반보드 열(세로줄) 생성, title과 year를 Body로 받아옵니다.")
+    @ApiErrorExceptionsExample(CreateColumnsExceptionDocs.class)
+    @PostMapping("/boards/navigations/{navigation-id}/columns")
+    public ResponseEntity<String> createBoardColumn(
+            @PathVariable("navigation-id") Integer navigationId,
+            @RequestBody CreateColumnsDto createColumnsDto) {
+        boardRecordUseCase.createColumnWithYear(navigationId, createColumnsDto);
+        return new ResponseEntity<>(COLUMN_SUCCESS_REGISTER_MESSAGE, HttpStatus.OK);
     }
 }
