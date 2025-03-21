1c2d03b788eca5bc77641974f64ef66a78a047f7
rlajm1203 <rlajm1203@naver.com>
2025-03-21 17:59:15 +0900

    feat: DefaultEmailProperties 클래스 정의
    - DefaultEmail에 들어갈 정보

---
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/email_template/util/DefaultEmailProperties.java
@@ -0,0 +1,24 @@
+package com.econovation.recruit.api.email_template.util;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class DefaultEmailProperties {
+
+    @Value("${econovation.recruit.open_chat_url}")
+    private String openChatUrl;
+
+    @Value("${econovation.recruit.open_chat_url_deadline}")
+    private LocalDateTime openChatUrlDeadLine;
+
+    @Value("${econovation.recruit.ot_schedule}")
+    private LocalDateTime otSchedule;
+
+    @Value("${econovation.recruit.ot_place}")
+    private String otPlace;
+
+}
