97f87a853a4b09bba3deb54e54808d18c25b092d
rlajm1203 <rlajm1203@naver.com>
2025-03-21 16:56:28 +0900

    feat: ApplicantEmailService 클래스 작성
    - applicant에게 이메일을 전송하는 역할

---
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/email/service/ApplicantEmailService.java
@@ -0,0 +1,66 @@
+package com.econovation.recruit.api.email.service;
+
+import com.econovation.recruit.api.email_template.util.DefaultTemplate;
+import com.econovation.recruitdomain.domains.applicant.domain.MongoAnswer;
+import com.econovation.recruitdomain.domains.applicant.domain.state.PassStates;
+import com.econovation.recruitinfrastructure.apache.CommonsEmailSender;
+import java.io.File;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ApplicantEmailService {
+
+    private final CommonsEmailSender emailSender;
+
+    private String openChatUrl;
+
+    private LocalDateTime urlDeadLine;
+
+    @Value("${econovation.file.path.portfolio}")
+    private String filePath;
+
+    public boolean sendEmail(MongoAnswer applicant) {
+        String template = generateEmailTemplate(applicant);
+        String subject = generateEmailSubject(applicant);
+        File attachment = getPortfolioFile(applicant);
+        String email = applicant.getQna().get("email").toString();
+
+        if(Objects.isNull(attachment))
+            return emailSender.sendEmail(email, subject, template);
+        else
+            if(attachment.exists())
+                return emailSender.sendEmailWithAttachment(email, subject, template, attachment);
+            else {
+                log.error("attachment 가 첨부되지 않았습니다. file dir : {}", attachment.getAbsolutePath());
+                return emailSender.sendEmail(email, subject, template);
+            }
+
+    }
+
+
+    private String generateEmailTemplate(MongoAnswer applicant) {
+        PassStates passState = applicant.getApplicantState().getPassStateToEnum();
+        return DefaultTemplate.getTemplate(passState).contextApply(applicant);
+    }
+
+    private String generateEmailSubject(MongoAnswer applicant){
+        PassStates passState = applicant.getApplicantState().getPassStateToEnum();
+        return DefaultTemplate.getTemplate(passState).getSubject();
+    }
+
+    private File getPortfolioFile(MongoAnswer applicant) {
+        // applicant에서 포트폴리오 파일 경로나 ID 등을 이용해 파일을 가져오는 로직
+        PassStates passState = applicant.getApplicantState().getPassStateToEnum();
+        if(passState==PassStates.FINAL_PASSED)
+            return new File(filePath);
+        return null;
+    }
+
+}
