6a1c372c468a5a7a11289be043619c0be12098b8
rlajm1203 <rlajm1203@naver.com>
2025-03-23 05:26:25 +0900

    refactor: Message 객체에 Getter 추가

--- server/Recruit-Infrastructure/src/main/java/com/econovation/recruitinfrastructure/ncp/NcpSmsDto.java
+++ server/Recruit-Infrastructure/src/main/java/com/econovation/recruitinfrastructure/ncp/NcpSmsDto.java
@@ -2,10 +2,12 @@ package com.econovation.recruitinfrastructure.ncp;
 
 import java.util.List;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
 public record NcpSmsDto(String type, String from, String subject, String content, List<Message> messages) {
 
+    @Getter
     public static class Message{
         private final String to;
 
