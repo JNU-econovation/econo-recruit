4c43892bb6913175fe46cd863f358ec14a704078
rlajm1203 <rlajm1203@naver.com>
2025-03-23 04:42:21 +0900

    feat: EmailSendEventHandler 정의
    - 이메일 발송 완료 이벤트 처리

---
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/sms/handler/EmailSendEventHandler.java
@@ -0,0 +1,37 @@
+package com.econovation.recruit.api.sms.handler;
+
+import com.econovation.recruit.api.applicant.usecase.ApplicantQueryUseCase;
+import com.econovation.recruit.api.sms.service.ApplicantSmsService;
+import com.econovation.recruitdomain.domains.applicant.domain.Applicant;
+import com.econovation.recruitdomain.domains.applicant.domain.MongoAnswer;
+import com.econovation.recruitdomain.domains.applicant.event.domainevent.ApplicantRegisterEvent;
+import com.econovation.recruitdomain.domains.email_template.event.EmailSendEvent;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class EmailSendEventHandler {
+
+    private final ApplicantSmsService smsService;
+
+    @Async
+    @TransactionalEventListener(
+            classes = EmailSendEvent.class,
+            phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void handle(EmailSendEvent emailSendEvent) {
+        String applicantId = emailSendEvent.getApplicantId();
+
+        smsService.sendSms(applicantId);
+    }
+
+}
