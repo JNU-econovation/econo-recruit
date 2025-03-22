401842e3c710efb566c7f66755c86fdd449a4f7f
rlajm1203 <rlajm1203@naver.com>
2025-03-23 04:42:49 +0900

    feat: sendSms(applicantId) 메소드 추가

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/sms/service/ApplicantSmsService.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/sms/service/ApplicantSmsService.java
@@ -4,6 +4,7 @@ import com.econovation.recruit.api.applicant.usecase.ApplicantQueryUseCase;
 import com.econovation.recruit.api.sms.util.SmsMessageGenerator;
 import com.econovation.recruitdomain.domains.applicant.domain.MongoAnswer;
 import com.econovation.recruitinfrastructure.ncp.NcpSmsSender;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,6 +25,28 @@ public class ApplicantSmsService {
     public void sendSms(MongoAnswer applicant){
         String phoneNumber = applicant.getQna().get("phone").toString();
         String name = applicant.getQna().get("name").toString();
+        String message = """
+                안녕하세요. %s님. 에코노베이션입니다.
+                
+                에코노베이션 %s기 신입모집 %s 결과가 나왔습니다.
+                
+                결과는 메일을 통해 확인해주시길 바랍니다.
+                
+                확인하시고 문자 회신 부탁드립니다.
+                """;
+
+        String finalMessage = String.format(message, name, year, messageGenerator.documentOrInterview());
+
+        boolean result = smsSender.sendSms(finalMessage, phoneNumber);
+
+        log.info("SMS RESULT : " + result);
+    }
+
+    public void sendSms(String applicantId){
+        Map<String,Object> qna = applicantQueryUseCase.execute(applicantId);
+
+        String phoneNumber = qna.get("phone").toString();
+        String name = qna.get("name").toString();
         String message = """
                 안녕하세요. %s님. 에코노베이션입니다.
                 
