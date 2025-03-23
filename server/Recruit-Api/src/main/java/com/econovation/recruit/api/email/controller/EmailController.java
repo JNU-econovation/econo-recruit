5f4fd63c5010b6e6cc1946ba71dd8d108a0edd1b
rlajm1203 <rlajm1203@naver.com>
2025-03-23 17:44:31 +0900

    feat: 이메일 API 컨트롤러 추가

---
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/email/controller/EmailController.java
@@ -0,0 +1,30 @@
+package com.econovation.recruit.api.email.controller;
+
+import com.econovation.recruit.api.applicant.usecase.ApplicantQueryUseCase;
+import com.econovation.recruit.api.email.service.ApplicantEmailService;
+import com.econovation.recruitdomain.domains.applicant.domain.MongoAnswer;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SecurityRequirement(name = "access-token")
+@RestController
+@RequestMapping("/api/v1")
+@Tag(name = "[8.0] email 관련 API", description = "메일 API")
+@RequiredArgsConstructor
+public class EmailController {
+    
+    private final ApplicantEmailService emailService;
+    private final ApplicantQueryUseCase applicantQueryUseCase;
+
+    @PostMapping("/emails/{applicantId}")
+    public ResponseEntity<String> send(@PathVariable String applicantId) {
+        return ResponseEntity.ok("");
+    }
+    
+}
