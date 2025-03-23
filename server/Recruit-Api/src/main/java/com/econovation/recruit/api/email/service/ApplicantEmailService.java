1cb4d6fcadcca9778762270b36f437775201ff5d
rlajm1203 <rlajm1203@naver.com>
2025-03-23 19:05:28 +0900

    feat: sendEmail(year, state) 메소드 추가

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/email/service/ApplicantEmailService.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/email/service/ApplicantEmailService.java
@@ -1,13 +1,22 @@
 package com.econovation.recruit.api.email.service;
 
+import com.econovation.recruit.api.applicant.usecase.ApplicantQueryUseCase;
 import com.econovation.recruit.api.email_template.util.DefaultEmailTemplateGenerator;
+import com.econovation.recruitdomain.common.aop.domainEvent.Events;
 import com.econovation.recruitdomain.domains.applicant.domain.MongoAnswer;
+import com.econovation.recruitdomain.domains.applicant.domain.state.PassStates;
+import com.econovation.recruitdomain.domains.email_template.event.EmailSendEvent;
 import com.econovation.recruitinfrastructure.apache.CommonsEmailSender;
 import java.io.File;
+import java.util.List;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +24,31 @@ import org.springframework.stereotype.Service;
 public class ApplicantEmailService {
 
     private final CommonsEmailSender emailSender;
+    private final ApplicantQueryUseCase applicantQueryUseCase;
     private final DefaultEmailTemplateGenerator templateGenerator;
+
+    @Value("${econovation.year}")
+    private int year;
+
+    @Async
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void sendEmail(int year, String state) {
+        PassStates target = PassStates.findStatus(state);
+        List<MongoAnswer> applicants = applicantQueryUseCase.getApplicantsByYear(year)
+                .stream().filter(applicant -> applicant.getApplicantState().getPassStateToEnum() == target)
+                .toList();
+
+        for (MongoAnswer applicant : applicants) {
+            boolean result = sendEmail(applicant);
+            if(!result) log.error("Email 발송 실패 : {}", applicant.getId());
+            else{
+                String applicantId = applicant.getId();
+                String passState = applicant.getApplicantState().getPassStateToEnum().name();
+
+                Events.raise(EmailSendEvent.of(applicantId, passState, ""));
+            }
+        }
+    }
 
     public boolean sendEmail(MongoAnswer applicant) {
         String template = templateGenerator.generateEmailTemplate(applicant);
