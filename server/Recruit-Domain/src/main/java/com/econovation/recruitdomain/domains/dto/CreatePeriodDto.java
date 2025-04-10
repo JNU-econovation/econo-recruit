0994ff3ada42e822a93407f001d3182e7e5731cb
rlajm1203 <rlajm1203@naver.com>
2025-04-10 14:39:57 +0900

    feat: Period 관련 Dto 정의

---
+++ server/Recruit-Domain/src/main/java/com/econovation/recruitdomain/domains/dto/CreatePeriodDto.java
@@ -0,0 +1,19 @@
+package com.econovation.recruitdomain.domains.dto;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreatePeriodDto {
+
+    private LocalDateTime recruitStart;
+    private LocalDateTime recruitEnd;
+    private LocalDateTime passDate;
+    private LocalDateTime firstDiscussionEnd;
+    private LocalDateTime finalDiscussionEnd;
+
+}
---
+++ server/Recruit-Domain/src/main/java/com/econovation/recruitdomain/domains/dto/PeriodResponseDto.java
@@ -0,0 +1,23 @@
+package com.econovation.recruitdomain.domains.dto;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PeriodResponseDto {
+
+    private LocalDateTime recruitStart;
+    private LocalDateTime recruitEnd;
+    private LocalDateTime passDate;
+    private LocalDateTime firstDiscussionEnd;
+    private LocalDateTime finalDiscussionEnd;
+
+    public static PeriodResponseDto of(LocalDateTime recruitStart, LocalDateTime recruitEnd, LocalDateTime passDate, LocalDateTime firstDiscussionEnd, LocalDateTime finalDiscussionEnd) {
+        return new PeriodResponseDto(recruitStart, recruitEnd, passDate, firstDiscussionEnd, finalDiscussionEnd);
+    }
+
+}
