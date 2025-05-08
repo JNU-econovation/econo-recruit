9a522e561f8ea00217fca839ccbcb3b8e40267bf
rlajm1203 <rlajm1203@naver.com>
2025-05-08 17:33:24 +0900

    feat: PeriodErrorCode 및 PeriodNotFoundException 정의

---
+++ server/Recruit-Domain/src/main/java/com/econovation/recruitdomain/domains/period/exception/PeriodErrorCode.java
@@ -0,0 +1,33 @@
+package com.econovation.recruitdomain.domains.period.exception;
+
+import static com.econovation.recruitcommon.consts.RecruitStatic.NOT_FOUND;
+
+import com.econovation.recruitcommon.annotation.ExplainError;
+import com.econovation.recruitcommon.exception.BaseErrorCode;
+import com.econovation.recruitcommon.exception.ErrorReason;
+import java.lang.reflect.Field;
+import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum PeriodErrorCode implements BaseErrorCode {
+    NOT_FOUND_PERIOD(NOT_FOUND, "PERIOD_404_1", "해당 모집 일정을 찾을 수 없습니다.");
+
+    private Integer status;
+    private String code;
+    private String reason;
+
+    @Override
+    public ErrorReason getErrorReason() {
+        return ErrorReason.builder().reason(reason).code(code).status(status).build();
+    }
+
+    @Override
+    public String getExplainError() throws NoSuchFieldException {
+        Field field = this.getClass().getField(this.name());
+        ExplainError annotation = field.getAnnotation(ExplainError.class);
+        return Objects.nonNull(annotation) ? annotation.value() : this.getReason();
+    }
+}
---
+++ server/Recruit-Domain/src/main/java/com/econovation/recruitdomain/domains/period/exception/PeriodNotFoundException.java
@@ -0,0 +1,13 @@
+package com.econovation.recruitdomain.domains.period.exception;
+
+import com.econovation.recruitcommon.exception.RecruitCodeException;
+
+public class PeriodNotFoundException extends RecruitCodeException {
+
+    public static final PeriodNotFoundException EXCEPTION = new PeriodNotFoundException();
+
+    public PeriodNotFoundException(){
+        super(PeriodErrorCode.NOT_FOUND_PERIOD);
+    }
+
+}
