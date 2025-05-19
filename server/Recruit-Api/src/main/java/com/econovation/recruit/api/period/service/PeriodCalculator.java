a529cc3840057438d8dadc5c418326af8a3c7e3e
rlajm1203 <rlajm1203@naver.com>
2025-05-19 14:50:25 +0900

    refactor: PeriodCalculator 내부 로직 수정
    - PeriodProvider 사용으로 변경

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/period/service/PeriodCalculator.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/period/service/PeriodCalculator.java
@@ -1,12 +1,19 @@
 package com.econovation.recruit.api.period.service;
 
 import com.econovation.recruitdomain.domains.applicant.domain.state.PeriodStates;
+import com.econovation.recruitdomain.domains.period.domain.Period;
+import com.econovation.recruitdomain.out.PeriodLoadPort;
 import java.time.LocalDateTime;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class PeriodCalculator {
+
+    private final PeriodProvider periodProvider;
 
     @Value("${econovation.recruit.period.firstDiscussion}")
     private String firstDiscussionPeriod;
@@ -15,12 +22,18 @@ public class PeriodCalculator {
     private String finalDiscussionPeriod;
 
     public PeriodStates execute() {
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime firstDiscussion = LocalDateTime.parse(firstDiscussionPeriod);
-        LocalDateTime finalDiscussion = LocalDateTime.parse(finalDiscussionPeriod);
+        Period period = periodProvider.get();
 
-        if (now.isBefore(firstDiscussion)) return PeriodStates.FIRST_DISCUSSION;
-        else if (now.isBefore(finalDiscussion)) return PeriodStates.FINAL_DISCUSSION;
-        else return PeriodStates.END;
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime firstDiscussionEnd = period.getFirstDiscussionEnd();
+        LocalDateTime finalDiscussionEnd = period.getFinalDiscussionEnd();;
+
+        // 설정된 기간과 현재 시간으로 어떤 기간인지 판별하기
+        if (now.isBefore(firstDiscussionEnd))
+            return PeriodStates.FIRST_DISCUSSION;
+        else if (now.isBefore(finalDiscussionEnd))
+            return PeriodStates.FINAL_DISCUSSION;
+        else
+            return PeriodStates.END;
     }
 }
