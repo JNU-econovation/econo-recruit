e3308772a207c79df0f9d6e706d20ac3962c7daa
Bellmin <whdals011203@naver.com>
2025-07-06 16:50:51 +0900

    refactor: Enum 상태 이름 변경
    - NON_START, RECRUITING, END

--- server/Recruit-Domain/src/main/java/com/econovation/recruitdomain/domains/applicant/domain/state/RecruitmentStates.java
+++ server/Recruit-Domain/src/main/java/com/econovation/recruitdomain/domains/applicant/domain/state/RecruitmentStates.java
@@ -2,6 +2,6 @@ package com.econovation.recruitdomain.domains.applicant.domain.state;
 
 public enum RecruitmentStates {
 
-    IMMEDIATELY_START, RESERVE_START, IMMEDIATELY_END, RESERVE_END;
+    NON_START, RECRUITING, END;
 
 }
