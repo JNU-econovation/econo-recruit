f9eaaf472354a47100084679b18a12921d110d86
Bellmin <whdals011203@naver.com>
2025-07-06 18:28:28 +0900

    refactor: 이벤트 핸들러 로깅 추가

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/recruitment/handler/RecruitmentEndEventHandler.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/recruitment/handler/RecruitmentEndEventHandler.java
@@ -6,10 +6,12 @@ import com.econovation.recruitdomain.domains.recruitment.event.RecruitmentEnd;
 import com.econovation.recruitdomain.out.RecruitmentPort;
 import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RecruitmentEndEventHandler {
@@ -22,12 +24,13 @@ public class RecruitmentEndEventHandler {
         Long id = event.getRecruitmentId();
 
         recruitmentPort.findById(id)
-                .ifPresent(end());
+                .ifPresentOrElse(end(), () -> log.error("id : {} 모집이 존재하지 않아 작업을 실패하였습니다.", id));
     }
 
     private Consumer<Recruitment> end(){
         return recruitment -> {
           recruitment.updateStates(RecruitmentStates.END);
+          recruitmentPort.save(recruitment);
         };
     }
 
--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/recruitment/handler/RecruitmentStartEventHandler.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/recruitment/handler/RecruitmentStartEventHandler.java
@@ -7,10 +7,12 @@ import com.econovation.recruitdomain.domains.recruitment.event.RecruitmentStart;
 import com.econovation.recruitdomain.out.RecruitmentPort;
 import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RecruitmentStartEventHandler {
@@ -23,7 +25,7 @@ public class RecruitmentStartEventHandler {
         Long id = event.getRecruitmentId();
 
         recruitmentPort.findById(id)
-                .ifPresent(start());
+                .ifPresentOrElse(start(),  () -> log.error("id : {} 모집이 존재하지 않아 작업을 실패하였습니다.", id));
     }
 
     private Consumer<Recruitment> start(){
