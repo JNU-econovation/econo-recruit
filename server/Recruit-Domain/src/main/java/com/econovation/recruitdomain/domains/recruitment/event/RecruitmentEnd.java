5dd72f6017d310e03e6e35207d8bef1cda111978
Bellmin <whdals011203@naver.com>
2025-07-06 17:38:53 +0900

    refactor: DomainEvent 상속

--- server/Recruit-Domain/src/main/java/com/econovation/recruitdomain/domains/recruitment/event/RecruitmentEnd.java
+++ server/Recruit-Domain/src/main/java/com/econovation/recruitdomain/domains/recruitment/event/RecruitmentEnd.java
@@ -1,11 +1,12 @@
 package com.econovation.recruitdomain.domains.recruitment.event;
 
+import com.econovation.recruitdomain.common.aop.domainEvent.DomainEvent;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class RecruitmentEnd {
+public class RecruitmentEnd extends DomainEvent {
 
     private final Long recruitmentId;
 
