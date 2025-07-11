71792d98c6df73a22614c88c8b8c0632f5ef324c
Bellmin <whdals011203@naver.com>
2025-07-11 10:17:27 +0900

    refactor: 스프링에서 제공하는 scheduler 사용

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/recruitment/quartz/RecruitmentScheduler.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/recruitment/quartz/RecruitmentScheduler.java
@@ -4,29 +4,22 @@ import com.econovation.recruit.api.recruitment.quartz.exception.QuartzException;
 import com.econovation.recruitdomain.domains.recruitment.domain.Recruitment;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobDetail;
 import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
-import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class RecruitmentScheduler {
 
     private static final ZoneId KST = ZoneId.of("Asia/Seoul");
 
     private final Scheduler scheduler;
-
-    public RecruitmentScheduler() {
-        try {
-            this.scheduler = new StdSchedulerFactory().getScheduler();
-        } catch (SchedulerException e){
-            throw new QuartzException(e);
-        }
-    }
 
     // DB에서 예약된 작업 불러오기
 //    @PostConstruct
@@ -47,7 +40,6 @@ public class RecruitmentScheduler {
         try {
             JobDetail jobDetail = RecruitmentJob.getStartJob(target.getId(), target.getYear());
 
-            scheduler.start();
             scheduler.scheduleJob(jobDetail, RecruitmentTrigger.get(jobDetail.getKey(), startAt(target)));
 
         } catch (SchedulerException e){
@@ -59,7 +51,6 @@ public class RecruitmentScheduler {
         try {
             JobDetail jobDetail = RecruitmentJob.getEndJob(target.getId(), target.getYear());
 
-            scheduler.start();
             scheduler.scheduleJob(jobDetail, RecruitmentTrigger.get(jobDetail.getKey(), endAt(target)));
 
         } catch (SchedulerException e){
