fd68df94cd3c8dca8a6921c753c611078ad93b02
Bellmin <whdals011203@naver.com>
2025-07-13 01:34:59 +0900

    refactor: LatestRecruitmentVo 유효성 검증 로직 수정

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/recruitment/util/LatestRecruitmentVo.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/recruitment/util/LatestRecruitmentVo.java
@@ -1,5 +1,6 @@
 package com.econovation.recruit.api.recruitment.util;
 
+import com.econovation.recruitdomain.common.aop.redissonLock.RedissonLock;
 import com.econovation.recruitdomain.domains.applicant.domain.state.RecruitmentStates;
 import com.econovation.recruitdomain.domains.recruitment.domain.Recruitment;
 import java.time.LocalDateTime;
@@ -8,32 +9,57 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-@AllArgsConstructor
+
 public class LatestRecruitmentVo {
 
-    private Recruitment latest;
+    private Integer year;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private RecruitmentStates state;
+
+    public LatestRecruitmentVo(Recruitment recruitment){
+        if(Objects.nonNull(recruitment)){
+            this.startAt = recruitment.getStartAt();
+            this.endAt = recruitment.getEndAt();
+            this.state = recruitment.getStates();
+            this.year = recruitment.getYear();
+        }
+    }
+
+    @RedissonLock(LockName = "모집 정보 최신화", identifier = "recruitment", paramClassType = Recruitment.class)
+    public void refreshRecruitment(Recruitment recruitment) {
+        if(Objects.nonNull(recruitment)){
+            this.startAt = recruitment.getStartAt();
+            this.endAt = recruitment.getEndAt();
+            this.state = recruitment.getStates();
+            this.year = recruitment.getYear();
+            return;
+        }
+        throw new IllegalArgumentException("최신 모집이 등록되지 않았습니다.");
+    }
 
     public Integer getYear() {
-        if (Objects.nonNull(latest)) return latest.getYear();
-        throw new IllegalArgumentException("최신 모집이 등록되지 않았습니다.");
+        validate();
+        return this.year;
     }
 
     public LocalDateTime getStartAt() {
-        if (Objects.nonNull(latest)) return latest.getStartAt();
-        throw new IllegalArgumentException("최신 모집이 등록되지 않았습니다.");
+        validate();
+        return this.startAt;
     }
 
     public LocalDateTime getEndAt() {
-        if (Objects.nonNull(latest)) return latest.getEndAt();
-        throw new IllegalArgumentException("최신 모집이 등록되지 않았습니다.");
+        validate();
+        return this.endAt;
     }
 
     public RecruitmentStates getState(){
-        if (Objects.nonNull(latest)) return latest.getStates();
-        throw new IllegalArgumentException("최신 모집이 등록되지 않았습니다.");
+        validate();
+        return this.state;
     }
 
-    public void refreshRecruitment(Recruitment recruitment) {
-        this.latest = recruitment;
+    private void validate(){
+        if (Objects.nonNull(year) || Objects.nonNull(startAt) || Objects.nonNull(endAt) || Objects.nonNull(state))
+            throw new IllegalArgumentException("최신 모집이 등록되지 않았습니다.");
     }
 }
