e0faaa49a3026b82eae7e48aa71174e38dbb62ed
Bellmin <whdals011203@naver.com>
2025-08-29 01:03:27 +0900

    feat: static 생성 함수 추가

--- server/Recruit-Domain/src/main/java/com/econovation/recruitdomain/domains/board/domain/Columns.java
+++ server/Recruit-Domain/src/main/java/com/econovation/recruitdomain/domains/board/domain/Columns.java
@@ -43,4 +43,14 @@ public class Columns extends BaseTimeEntity {
     public void updateNextColumnsId(Integer id) {
         this.nextColumnsId = id;
     }
+
+    /**
+     * 공통 Navigation에 있는 컬럼을 생성하므로, navigationId 는 1로 설정됩니다.
+     * @param title
+     * @param year
+     * @return
+     */
+    public static Columns createCommonColumn(String title, int year){
+        return Columns.builder().navigationId(1).title(title).year(year).build();
+    }
 }
