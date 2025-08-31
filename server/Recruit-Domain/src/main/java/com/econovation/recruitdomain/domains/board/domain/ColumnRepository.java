4ddbc49d3c1379b6fe3e5cb584cd47a917e0a3c7
Bellmin <whdals011203@naver.com>
2025-08-31 18:22:17 +0900

    refactor: param 어노테이션 추가

--- server/Recruit-Domain/src/main/java/com/econovation/recruitdomain/domains/board/domain/ColumnRepository.java
+++ server/Recruit-Domain/src/main/java/com/econovation/recruitdomain/domains/board/domain/ColumnRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,7 +12,7 @@ public interface ColumnRepository extends JpaRepository<Columns, Integer> {
     List<Columns> findByNavigationId(Integer navigationId);
 
     @Query("SELECT c FROM Columns c WHERE c.navigationId = :navigationId AND c.year = :year")
-    List<Columns> findByNavigationIdAndYear(Integer navigationId, Integer year);
+    List<Columns> findByNavigationIdAndYear(@Param("navigationId") Integer navigationId, @Param("year") Integer year);
 
     Optional<Columns> findByNextColumnsIdAndNavigationId(Integer nextColLoc, Integer navigationId);
 
@@ -19,8 +20,8 @@ public interface ColumnRepository extends JpaRepository<Columns, Integer> {
 
     @Query(
             "select case when count(c) > 0 then true else false end from Columns c where c.title = :title and c.year = :year ")
-    boolean existsByTitleAndYear(String title, Integer year);
+    boolean existsByTitleAndYear(@Param("title") String title, @Param("year") Integer year);
 
     @Query("SELECT c FROM Columns c WHERE c.title=:title AND c.year=:year")
-    Optional<Columns> findByTitleAndYear(String title, Integer Year);
+    Optional<Columns> findByTitleAndYear(@Param("title") String title, @Param("year") Integer Year);
 }
