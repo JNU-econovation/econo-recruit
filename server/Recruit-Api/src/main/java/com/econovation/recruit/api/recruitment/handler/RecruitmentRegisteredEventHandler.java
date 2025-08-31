0835c2df79c05769ae2b08ffa40e71ac6b560f0f
Bellmin <whdals011203@naver.com>
2025-08-31 16:54:15 +0900

    style: spotlessApply

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/recruitment/handler/RecruitmentRegisteredEventHandler.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/recruitment/handler/RecruitmentRegisteredEventHandler.java
@@ -15,7 +15,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
