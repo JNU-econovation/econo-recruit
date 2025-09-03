e4c5dd22630768eaa7dc2931c63b171e9a5fef40
westzeroright <kt8267@naver.com>
2025-09-03 21:24:08 +0900

    [fix]: @Valid 어노테이션 추가
    해당 부분을 생략하여 검증 시에 200 상태 코드가 오는 문제가 있었습니다.

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/user/controller/UserController.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/user/controller/UserController.java
@@ -132,7 +132,7 @@ public class UserController {
 
     @Operation(summary = "회원가입 시 이메일 인증", description = "회원가입 시 유효한 메일인지 확인하기 위해 이메일 인증합니다.")
     @PostMapping("/signup/verify")
-    public ResponseEntity sendEmailForSignup(@RequestBody SendEmailRequestDto sendEmailRequestDto) {
+    public ResponseEntity sendEmailForSignup(@Valid @RequestBody SendEmailRequestDto sendEmailRequestDto) {
         sendEmailUseCase.sendEmailForSignup(sendEmailRequestDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
