2a947cebddc2d0d711751b062aae0f54d726f037
westzeroright <kt8267@naver.com>
2025-09-11 17:17:09 +0900

    [feat]: 관리자만 해당 기능을 사용할 수 있도록 설정

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/config/security/SecurityConfig.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/config/security/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig {
                 .hasAnyRole("ROLE_OPERATION", "ROLE_PRESIDENT")
                 .mvcMatchers(HttpMethod.POST, "/api/v1/recruitment")
                 .hasAnyRole("ROLE_OPERATION", "ROLE_PRESIDENT")
-                .mvcMatchers(HttpMethod.DELETE, "/api/v1/applicants/all/*")
+                .mvcMatchers(HttpMethod.DELETE, "/api/v1/applicants/all/*", "/api/v1/applicants")
                 .hasAnyRole("ROLE_OPERATION")
                 .anyRequest()
                 .hasAnyRole(RolePattern);
