f1741d1c609e09636799caadf4d19a4674b0a84d
westzeroright <westzeroright@gmail.com>
2026-02-15 20:11:18 +0900

    refactor: 용어 통일

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/comment/controller/CommentController.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/comment/controller/CommentController.java
@@ -97,7 +97,7 @@ public class CommentController {
         return new ResponseEntity<>(COMMENT_LIKE_SUCCESS_DELETE_MESSAGE, HttpStatus.OK);
     }
 
-    @Operation(summary = "코멘트 전체 공개", description = "회장단만 사용 가능합니다.")
+    @Operation(summary = "댓글 전체 공개", description = "회장단만 사용 가능합니다.")
     @PostMapping("/comments/disclosure")
     public ResponseEntity changeDisclosure() {
         commentUseCase.changeViewMode();
