ead6a19bea358fffacdfd91bd736b81330f7f397
westzeroright <westzeroright@gmail.com>
2026-02-20 00:28:36 +0900

    [feat]: 스웨거 요청 형식 지정

--- server/Recruit-Domain/src/main/java/com/econovation/recruitdomain/domains/dto/RecruitmentSetUpDto.java
+++ server/Recruit-Domain/src/main/java/com/econovation/recruitdomain/domains/dto/RecruitmentSetUpDto.java
@@ -1,5 +1,6 @@
 package com.econovation.recruitdomain.domains.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -13,8 +14,10 @@ public class RecruitmentSetUpDto {
     private Integer year;
 
     @NotNull
+    @Schema(type = "string", example = "2026-02-14T23:38:00")
     private LocalDateTime startAt;
 
     @NotNull
+    @Schema(type = "string", example = "2026-02-15T19:30:00")
     private LocalDateTime endAt;
 }
