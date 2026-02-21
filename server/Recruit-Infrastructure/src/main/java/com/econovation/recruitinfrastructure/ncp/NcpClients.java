769cda7f5613c8d30a49143190c1b3455b117bd9
LeeJaeRyun <pinixs3@naver.com>
2026-02-22 02:12:51 +0900

    [FIX]: FeignClient name 충돌로 인한 Bean 중복 등록 문제 해결

--- server/Recruit-Infrastructure/src/main/java/com/econovation/recruitinfrastructure/ncp/NcpClients.java
+++ server/Recruit-Infrastructure/src/main/java/com/econovation/recruitinfrastructure/ncp/NcpClients.java
@@ -10,7 +10,11 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 public class NcpClients {
 
-    @FeignClient(name = "NcpClient", url = "${ncp.mail-base-url}", configuration = NcpConfig.class)
+    @FeignClient(
+            name = "NcpMailClient",
+            contextId = "ncpMailClient",
+            url = "${ncp.mail-base-url}",
+            configuration = NcpConfig.class)
     @Headers("Content-Type: application/json; charset=UTF-8")
     public interface NcpMailClient {
         @PostMapping(path = "${ncp.mail-api-url}", consumes = "application/json; charset=UTF-8")
@@ -22,7 +26,11 @@ public class NcpClients {
                 @RequestBody SendRawEmailDto sendRawEmailDto);
     }
 
-    @FeignClient(name = "NcpClient", url = "${ncp.sms-base-url}", configuration = NcpConfig.class)
+    @FeignClient(
+            name = "NcpSmsClient",
+            contextId = "ncpSmsClient",
+            url = "${ncp.sms-base-url}",
+            configuration = NcpConfig.class)
     @Headers("Content-Type: application/json; charset=UTF-8")
     public interface NcpSmsClient {
         @PostMapping(path = "${ncp.sms-api-url}", consumes = "application/json;")
