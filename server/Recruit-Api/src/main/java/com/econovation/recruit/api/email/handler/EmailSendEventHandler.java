6e24751cf66423117497a47c120b9528e7457539
Bellmin <whdals011203@naver.com>
2026-03-13 00:29:22 +0900

    [feat]: 이메일 발송 성공 시 Slack 알림을 이벤트 기반으로 처리

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/email/handler/EmailSendEventHandler.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/email/handler/EmailSendEventHandler.java
@@ -2,6 +2,9 @@ package com.econovation.recruit.api.email.handler;
 
 import com.econovation.recruit.api.sms.service.ApplicantSmsService;
 import com.econovation.recruitdomain.domains.email_template.event.EmailSendEvent;
+import com.econovation.recruitinfrastructure.slack.SlackMessageProvider;
+import com.econovation.recruitinfrastructure.slack.config.SlackProperties;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -14,12 +17,34 @@ import org.springframework.stereotype.Component;
 public class EmailSendEventHandler {
 
     private final ApplicantSmsService smsService;
+    private final SlackMessageProvider slackMessageProvider;
+    private final SlackProperties slackProperties;
 
     @Async
     @EventListener(EmailSendEvent.class)
     public void handle(EmailSendEvent emailSendEvent) {
-        String applicantId = emailSendEvent.getApplicantId();
+        smsService.sendSms(emailSendEvent.getApplicantId());
 
-        smsService.sendSms(applicantId);
+        if (emailSendEvent.isSlackNotify()) {
+            String url =
+                    Objects.isNull(emailSendEvent.getSlackUrl())
+                            ? slackProperties.getUrl()
+                            : emailSendEvent.getSlackUrl();
+            slackMessageProvider.sendMessage(url, generateSlackMessage(emailSendEvent));
+        }
+    }
+
+    private String generateSlackMessage(EmailSendEvent event) {
+        return String.format(
+                """
+                [메일 발송 성공]
+                - 이름 : %s
+                - 지원 분야 : %s / %s
+                - 합격 상태 : %s
+                """,
+                event.getName(),
+                event.getField1(),
+                event.getField2(),
+                event.getEmailTemplateType().name());
     }
 }
