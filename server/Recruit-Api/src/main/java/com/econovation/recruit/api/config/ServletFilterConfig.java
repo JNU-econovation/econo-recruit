2ff9049a4713fbe371502212383cbb9ee8a8bdfa
LeeJaeRyun <pinixs3@naver.com>
2026-08-25 19:53:39 +0900

    Revert "fix: Spring Security 필터 중복 등록 제거"
    This reverts commit b4c63cc942a828c32b68a5cc2b81bd2036b68ca1.

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/config/ServletFilterConfig.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/config/ServletFilterConfig.java
@@ -1,10 +1,13 @@
 package com.econovation.recruit.api.config;
 
+import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
@@ -16,6 +19,16 @@ public class ServletFilterConfig implements WebMvcConfigurer {
 
     private final HttpContentCacheFilter httpContentCacheFilter;
     private final ForwardedHeaderFilter forwardedHeaderFilter;
+
+    @Bean
+    public FilterRegistrationBean securityFilterChain(
+            @Qualifier(AbstractSecurityWebApplicationInitializer.DEFAULT_FILTER_NAME)
+                    Filter securityFilter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean(securityFilter);
+        registration.setOrder(Integer.MAX_VALUE - 3);
+        registration.setName(AbstractSecurityWebApplicationInitializer.DEFAULT_FILTER_NAME);
+        return registration;
+    }
 
     @Bean
     public FilterRegistrationBean setResourceUrlEncodingFilter() {
